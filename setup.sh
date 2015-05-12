70a0300433c663bf3e089fd9f5490c8d94b49e8c
Vlad Balmos <vlad.balmos@gmail.com>
2015-05-12 22:42:02 +0300

    Fixed misspelling

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@
 git submodule update --init
 CURRENT_PATH=$(pwd)
 ln -s $CURRENT_PATH/vimrc $HOME/.vimrc
-echo -e "\033[0;31mDon't forger to run :Helptags\033[0m"
+echo -e "\033[0;31mDon't forget to run :Helptags\033[0m"
