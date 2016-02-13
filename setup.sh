65cee2d0379e02bd6591e734e649cfe14b1478a2
Vlad Balmos <vlad.balmos@gmail.com>
2016-02-13 20:36:03 +0200

    Update setup

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.v
 CURRENT_PATH=$(pwd)
 ln -fs $CURRENT_PATH/vimrc $HOME/.vimrc
 ln -fs $CURRENT_PATH/vimrc init.vim
+vim +PluginInstall +qall
