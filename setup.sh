1fd6d766e7263515265a0936ebb2ded9618c1d43
Vlad Balmos <vlad.balmos@code932.com>
2016-02-17 14:52:34 +0200

    Add ycm plugin & update setup

--- setup.sh
+++ setup.sh
@@ -5,3 +5,5 @@ ln -fs vimrc init.vim
 cd $HOME && ln -fs .vim/vimrc .vimrc
 cd $HOME/.config && ln -fs $HOME/.vim nvim
 vim +PluginInstall +qall
+cd $HOME/.vim/bundle/YouCompleteMe && git submodule update --init --recursive && ./install.py --tern-completer
+
